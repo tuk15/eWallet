d63134852c54d2122eecabba83a6dfa8dd0d7a68
colintu666 <colintu.kk@gmail.com>
2019-03-25 21:13:33 -0400

    First commit

---
+++ app/src/main/java/com/cis436/paypalapidemo/Config/Config.java
@@ -0,0 +1,5 @@
+package com.cis436.paypalapidemo.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID="Af6qRBx6zErU5U_mO4fGf5kHtelY6WckTwsKeYYiamtDwWJVEnkkn3oBN52OnfvsPCveJkjVf06tsOLG";
+}
